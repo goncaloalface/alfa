5ca87c304d06b95b80809fc09fba9a455ace6f50
Gonçalo Alface <56477403+goncaloalface@users.noreply.github.com>
2021-02-20 17:49:48 +0000

    Update settings.py

--- Alfa/settings.py
+++ Alfa/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-zryol=dplysfwf-g+hp-&hqur@4dd4#kpq-8aa-b4528v@c$0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
